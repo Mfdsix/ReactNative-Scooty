d9da09c26233730b1dea73fea24f2ac554aa8cf2
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-10-16 11:21:51 +0700

    register splash and trip to routes

--- src/Routes.js
+++ src/Routes.js
@@ -2,15 +2,19 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from './screens/Home';
+import SplashScreen from './screens/Splash';
+import TripScreen from './screens/Trip';
 
 const Stack = createNativeStackNavigator();
 
 const Routes = () => (
   <NavigationContainer>
     <Stack.Navigator
-      initialRouteName="Home"
+      initialRouteName="Splash"
       screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Splash" component={SplashScreen} />
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Trip" component={TripScreen} />
     </Stack.Navigator>
   </NavigationContainer>
 );
